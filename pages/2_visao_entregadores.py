7f8585907c736caef5dbe1747130637c935879d7
Marcelo Rissi <marcelorissi2@gmail.com>
2023-09-18 20:04:07 -0300

    date adjustment

--- pages/2_visao_entregadores.py
+++ pages/2_visao_entregadores.py
@@ -110,7 +110,7 @@ st.sidebar.markdown("""---""")
 st. sidebar.markdown('## Selecione uma data limite')
 data_slider = st.sidebar.slider(
     'Até qual valor?',
-    value=pd.datetime( 2022, 4, 13 ),
+    value=pd.datetime( 2024, 4, 13 ),
     min_value=pd.datetime(2022, 2, 11),
     max_value=pd.datetime(2022, 4, 6),
     format='DD-MM-YYYY')
